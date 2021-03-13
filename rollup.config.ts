7260c4cd4910017ef3dd85be8cc552452b4615a3
Adenekan Wonderful <adenekanwonderful41@gmail.com>
2021-03-13 17:51:01 +0100

    feat: Initaited package
    BREAKING CHANGE: Rollup TS

---
+++ rollup.config.ts
@@ -0,0 +1,32 @@
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import sourceMaps from 'rollup-plugin-sourcemaps'
+import camelCase from 'lodash.camelcase'
+import typescript from 'rollup-plugin-typescript2'
+import json from 'rollup-plugin-json'
+
+const pkg = require('./package.json')
+
+const libraryName = 'use-responsive-query'
+
+export default {
+  external: ['react'],
+  input: `src/${libraryName}.ts`,
+  output: [
+    { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
+    { file: pkg.module, format: 'es', sourcemap: true },
+  ],
+  watch: {
+    include: 'src/**',
+  },
+  globals: {
+    react: 'React',
+  },
+  plugins: [
+    json(),
+    typescript({ useTsconfigDeclarationDir: true }),
+    commonjs(),
+    resolve(),
+    sourceMaps(),
+  ],
+}
